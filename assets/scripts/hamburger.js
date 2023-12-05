24bde8b983ace694f8912d12613fd054f13fb3fa
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2023-12-05 11:37:22 +0100

    05/12: Optimalizeren navbar, design veranderingen.

--- assets/scripts/hamburger.js
+++ assets/scripts/hamburger.js
@@ -1,6 +1,11 @@
-
-
 function toggleMobileMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('nav-links-active');
+    navLinks.classList.toggle('nav-links-inactive');
+    
+    if (navLinks.classList.contains('nav-links-active')) {
+        navLinks.classList.remove('nav-links-inactive');
+    } else {
+        navLinks.classList.remove('nav-links-active');
+    }
 }
